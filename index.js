093d0cdb6ca1df86af5c3c35ad59add1013d1fe2
guylev008 <guy@spectralops.io>
2024-12-16 10:54:20 +0200

    add json-v2 to spectral command

--- index.js
+++ index.js
@@ -73,5 +73,5 @@ async function runSpectral() {
 
 function getScanCommand() {
     const spectralArgs = core.getInput('spectral-args')
-    return `${process.platform === 'win32' ? 'spectral.exe' : 'spectral'} ${spectralArgs}`
+    return `${process.platform === 'win32' ? 'spectral.exe' : 'spectral'} ${spectralArgs} --json-v2 /dev/stderr`
 }
