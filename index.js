caa11c95aea41da42b1742b4e482563140b327f8
guylev008 <guy@spectralops.io>
2024-12-16 11:01:48 +0200

    remove json v2

--- index.js
+++ index.js
@@ -74,5 +74,5 @@ async function runSpectral() {
 
 function getScanCommand() {
     const spectralArgs = core.getInput('spectral-args')
-    return `${process.platform === 'win32' ? 'spectral.exe' : 'spectral'} ${spectralArgs} --json-v2 /dev/stderr`
+    return `${process.platform === 'win32' ? 'spectral.exe' : 'spectral'} ${spectralArgs}`
 }
